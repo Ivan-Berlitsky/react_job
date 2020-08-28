539ec3e7031d1636da4ddf27611f0a2c773d6038
Ivan Berlitsky <66734660+Ivan-Berlitsky@users.noreply.github.com>
2020-08-29 06:02:39 +0800

    add img, add post(component)

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -1,29 +1,24 @@
 import React from 'react';
+import Post from './Posts/Post';
 import classes from './Content.module.css';
 
+
 const Profile = () => {
-    return <div className={classes.content}>
-    <div className={classes.imgContain}>
-      <img src='https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&h=350' />
-    </div>
-    <div>
-      ava + description
-    </div>
-    <div>
-      My posts
-      <div>
-        New post
+  return (
+    <div className={classes.content}>
+      <div className={classes.imgContain}>
+        <img src='https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&h=350' />
       </div>
       <div>
-        <div>
-          post 1
-        </div>
-        <div>
-          post 2
-        </div>
+        ava + description
       </div>
+      <div>
+        My posts
+      </div>
+      <Post name='Евгения Берлицкая' score='23' text="loremLorem ipsum dolor sit amet consectetur adipisicing elit. Minus id ut tempora assumenda? Asperiores tenetur adipisci sed facere doloremque fuga tempore amet, commodi repellat? Repellat voluptate ducimus minima dolore. Aperiam." />
+      <Post name='Андрей Берлицкий' score='54' text="loremLorem ipsum dolor sit amet consectetur adipisicing elit. Minus id ut tempora assumenda? Asperiores tenetur adipisci sed facere doloremque fuga tempore amet, commodi repellat? Repellat voluptate ducimus minima dolore. Aperiam.loremLorem ipsum dolor sit amet consectetur adipisicing elit. Minus id ut tempora assumenda? Asperiores tenetur adipisci sed facere doloremque fuga tempore amet, commodi repellat? Repellat voluptate ducimus minima dolore. Aperiam.loremLorem ipsum dolor sit amet consectetur adipisicing elit. Minus id ut tempora assumenda? Asperiores tenetur adipisci sed facere doloremque fuga tempore amet, commodi repellat? Repellat voluptate ducimus minima dolore. Aperiam.loremLorem ipsum dolor sit amet consectetur adipisicing elit. Minus id ut tempora assumenda? Asperiores tenetur adipisci sed facere doloremque fuga tempore amet, commodi repellat? Repellat voluptate ducimus minima dolore. Aperiam.loremLorem ipsum dolor sit amet consectetur adipisicing elit. Minus id ut tempora assumenda? Asperiores tenetur adipisci sed facere doloremque fuga tempore amet, commodi repellat? Repellat voluptate ducimus minima dolore. Aperiam." />
     </div>
-  </div>
-}
+    );  
+};
 
 export default Profile;
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,23 +1,32 @@
 import React from 'react';
-import classes from './Navigation.module.css';
-import profile from './../../assets/images/profile.svg';
+import Classes from './Navigation.module.css';
+import Profile from './../../assets/images/profile.svg';
+import Message from './../../assets/images/message.svg';
+import News from './../../assets/images/news.svg';
+import Music from './../../assets/images/music.svg';
+import Settings from './../../assets/images/settings.svg';
 const Navbar = () => {
-    return <nav className={classes.nav}>
-        <div>
-            <object data={profile} type="image/svg+xml" height='20' width= '20'></object>
-            <a>Profile</a>
-        </div>
-        <div>
-            <a>Messages</a>
-        </div><div>
-            <a>News</a>
-        </div>
-        <div>
-            <a>Music</a>
-        </div>
-        <div>
-            <a>Settings</a>
-        </div>
+    return <nav className={Classes.nav}>
+        <button className={Classes.navButton}>
+            <object data={Profile} type="image/svg+xml" height='20' width= '20'></object>
+            <a className={Classes.navMenu}>Profile</a>
+        </button>
+        <button className={Classes.navButton}>
+            <object data={Message} type="image/svg+xml" height='20' width= '20'></object>
+            <a className={Classes.navMenu}>Messages</a>
+        </button>
+        <button className={Classes.navButton}>
+            <object data={News} type="image/svg+xml" height='20' width= '20'></object>
+            <a className={Classes.navMenu}>News</a>
+        </button>
+        <button className={Classes.navButton}>
+            <object data={Music} type="image/svg+xml" height='20' width= '20'></object>
+            <a className={Classes.navMenu}>Music</a>
+        </button>
+        <button className={Classes.navButton}>
+            <object data={Settings} type="image/svg+xml" height='20' width= '20'></object>
+            <a className={Classes.navMenu}>Settings</a>
+        </button>
     </nav>
 }
 
