1ee34161f9acf863002f9c9c774bb9b26ae9d973
Ivan_Berlitsky <ang.iv@mail.ru>
2020-09-08 15:04:31 +0800

    work on bugs

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -11,23 +11,23 @@ import Settings from './../../assets/images/settings.svg';
 const Navbar = () => {
     return <nav className={Classes.nav}>
         <button className={Classes.navButton}>
-            <object data={Profile} type="image/svg+xml" height='20' width= '25'></object>
+            <object data={Profile} type="image/svg+xml" height='20' width= '25' aria-labelledby='profile'></object>
             <NavLink to='/profile' className={Classes.navMenu} activeClassName={Classes.activeLink}>Profile</NavLink>
         </button>
         <button className={Classes.navButton}>
-            <object data={Message} type="image/svg+xml" height='20' width= '25'></object>
+            <object data={Message} type="image/svg+xml" height='20' width= '25' aria-labelledby='messages'></object>
             <NavLink to='/messages' className={Classes.navMenu} activeClassName={Classes.activeLink}>Messages</NavLink>
         </button>
         <button className={Classes.navButton}>
-            <object data={News} type="image/svg+xml" height='20' width= '25'></object>
+            <object data={News} type="image/svg+xml" height='20' width= '25' aria-labelledby='news'></object>
             <NavLink to='/news' className={Classes.navMenu} activeClassName={Classes.activeLink}>News</NavLink>
         </button>
         <button className={Classes.navButton}>
-            <object data={Music} type="image/svg+xml" height='20' width= '25'></object>
+            <object data={Music} type="image/svg+xml" height='20' width= '25' aria-labelledby='music'></object>
             <NavLink to='/music' className={Classes.navMenu} activeClassName={Classes.activeLink}>Music</NavLink>
         </button>
         <button className={Classes.navButton}>
-            <object data={Settings} type="image/svg+xml" height='20' width= '25'></object>
+            <object data={Settings} type="image/svg+xml" height='20' width= '25' aria-labelledby='settings'></object>
             <NavLink to='/settings' className={Classes.navMenu} activeClassName={Classes.activeLink}>Settings</NavLink>
         </button>
     </nav>
--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -8,7 +8,7 @@ const Profile = (props) => {
   return (
     <div className={classes.content}>
       <div className={classes.imgContain}>
-        <img src='https://i.pinimg.com/originals/ba/6c/d4/ba6cd45d22f01b59c1c4605c4e6dabab.jpg' alt="image"/>
+        <img src='https://i.pinimg.com/originals/ba/6c/d4/ba6cd45d22f01b59c1c4605c4e6dabab.jpg' alt="wallpaper"/>
       </div>
       <div>
         ava + description
